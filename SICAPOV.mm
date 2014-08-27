2f804cae940aa790f74c2ae6655c5417c58fed1e
pachos7 <franciscojny@gmail.com>
2014-08-27 17:28:32 -0500

    Mind Map added

---
+++ SICAPOV.mm
@@ -0,0 +1,44 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1409094389628" ID="ID_760435741" MODIFIED="1409094507383" TEXT="SICAPOV">
+<node CREATED="1409094896778" ID="ID_1826411447" MODIFIED="1409094900598" POSITION="right" TEXT="Tasks">
+<node CREATED="1409094908959" ID="ID_489764169" MODIFIED="1409094921418" TEXT="Create Test enviroment">
+<node CREATED="1409094925533" ID="ID_864300581" MODIFIED="1409094959129" TEXT="N. We would like the server to be a LInux Machine"/>
+<node CREATED="1409095586752" ID="ID_38788104" MODIFIED="1409095619716" TEXT="Create a local web server on CMNYVM">
+<node CREATED="1409095635841" ID="ID_517527778" MODIFIED="1409095672665" TEXT="Install VirtualBox">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1409095645310" ID="ID_1295683572" MODIFIED="1409095674352" TEXT="Install CS50 Appliance">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1409095653691" ID="ID_1807301186" MODIFIED="1409095682571" TEXT="Config the Apache to serve new files">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1409095586752" ID="ID_3832404" MODIFIED="1409095627464" TEXT="Create a local web server on MFNYVM">
+<node CREATED="1409095635841" ID="ID_651920467" MODIFIED="1409095686522" TEXT="Install VirtualBox">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1409095645310" ID="ID_1346342314" MODIFIED="1409095687998" TEXT="Install CS50 Appliance">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1409095653691" ID="ID_447306372" MODIFIED="1409095690559" TEXT="Config the Apache to serve new files">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1409100437882" ID="ID_690387566" MODIFIED="1409100443208" TEXT="Setup code sharing">
+<node CREATED="1409097779760" ID="ID_629028082" MODIFIED="1409100417830" TEXT="Create a repository in GIT">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1409097795741" ID="ID_908307433" MODIFIED="1409100455796" TEXT="Install git client on CMNY">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1409097795741" ID="ID_1142669510" MODIFIED="1409100455797" TEXT="Install git client on MFNY">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1409100422892" ID="ID_351823281" MODIFIED="1409100432833" TEXT="Move curren code to GIT"/>
+</node>
+</node>
+</node>
+</node>
+</map>
